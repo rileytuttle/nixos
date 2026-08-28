9209e1c9b1767ad14698fd17c3f9df60982e8469
Riley Tuttle <rileytuttle@gmail.com>
2026-08-27 21:30:05 -0400

    wip

---
+++ home/dank-material-shell.nix
@@ -0,0 +1,26 @@
+# fw12-only home-manager module (wired in flake.nix, not home/default.nix).
+# Separate from home/niri.nix so DMS itself can be turned off independently
+# of the niri session, in case it doesn't work out.
+{ inputs, ... }:
+
+{
+  imports = [ inputs.dms.homeModules.dank-material-shell ];
+
+  programs.dank-material-shell = {
+    enable = true;
+
+    # Autostarted by the `spawn-at-startup "dms" "run"` line in
+    # home/niri-config.kdl instead of via systemd, so leave this off to
+    # avoid spawning it twice.
+    systemd.enable = false;
+
+    enableSystemMonitoring = true;
+    enableDynamicTheming = true;
+    enableAudioWavelength = true;
+
+    settings = {
+      theme = "dark";
+      dynamicTheming = true;
+    };
+  };
+}
